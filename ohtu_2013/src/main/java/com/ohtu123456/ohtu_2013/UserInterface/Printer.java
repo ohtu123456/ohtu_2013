7e0dfa0b593ca2d58460c02bdfc1e863d8d93ad8
Heikki Kalliokoski <heikki.kalliokoski@helsinki.fi>
2013-04-08 20:14:01 +0300

    Korjaus luokan nimeen.

--- ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/UserInterface/Printer.java
+++ ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/UserInterface/Printer.java
@@ -4,6 +4,6 @@ package com.ohtu123456.ohtu_2013.UserInterface;
  *
  * @author Heikki Kalliokoski
  */
-public interface Writer {
+public interface Printer {
     public void println(String message);
 }

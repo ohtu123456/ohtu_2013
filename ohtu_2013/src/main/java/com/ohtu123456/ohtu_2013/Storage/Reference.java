b4c169e91a94eed9f56e7d7afb47c780adbfa326
Heikki Kalliokoski <heikki.kalliokoski@helsinki.fi>
2013-04-08 17:33:17 +0300

    Lisätty ReferenceInterface -rajapinta sekä määritelty Reference -luokka toteuttamaan se.

--- ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/Storage/Reference.java
+++ ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/Storage/Reference.java
@@ -3,7 +3,7 @@ package com.ohtu123456.ohtu_2013.Storage;
 import java.util.HashMap;
 import java.util.Map;
 
-class Reference {
+class Reference implements ReferenceInterface {
     
     private Map<String, String> ref;
     
@@ -11,12 +11,14 @@ class Reference {
         ref = new HashMap<String, String>();
     }
 
+    @Override
     public String getField(String field) {
         if(!ref.containsKey(field))
             return "";
         return ref.get(field);
     }
 
+    @Override
     public void setField(String field, String value) {
         ref.put(field, value);
     }
---
+++ ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/Storage/ReferenceInterface.java
@@ -0,0 +1,11 @@
+package com.ohtu123456.ohtu_2013.Storage;
+
+/**
+ *
+ * @author Heikki Kalliokoski
+ */
+public interface ReferenceInterface {
+
+    String getField(String field);
+    void setField(String field, String value);
+}

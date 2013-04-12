cba63658e1292bd00d058382f7cfc1bb956e4e5a
Heikki Kalliokoski <heikki.kalliokoski@helsinki.fi>
2013-04-12 11:15:49 +0300

    Lisättiin Printteri-luokkaan print-metodi.

--- ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/UserInterface/PrinterSystemOut.java
+++ ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/UserInterface/PrinterSystemOut.java
@@ -12,5 +12,9 @@ public class PrinterSystemOut implements Printer{
     public void println(String message) {
         System.out.println(message);
     }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
     
 }

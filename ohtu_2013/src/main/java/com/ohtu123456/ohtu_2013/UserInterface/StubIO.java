78849c2c9a6bf0368313298723eb8ebffb0e8dec
Heikki Kalliokoski <heikki.kalliokoski@helsinki.fi>
2013-04-19 12:24:30 +0300

    Lisätty StubIO.

---
+++ ohtu_2013/src/main/java/com/ohtu123456/ohtu_2013/UserInterface/StubIO.java
@@ -0,0 +1,35 @@
+package com.ohtu123456.ohtu_2013.UserInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author Heikki Kalliokoski
+ */
+public class StubIO implements IO{
+    
+    private List<String> output;
+    private List<String> input;
+    private Iterator<String> outputIterator;
+    
+    public StubIO(String... args){
+        output = new ArrayList<String>();
+        input = new ArrayList<String>();
+        for(String arg: args){
+            output.add(arg);
+        }
+        outputIterator = output.iterator();
+    }
+
+    public void println(String line) {
+        input.add(line);
+    }
+
+    public String nextLine() {
+        return outputIterator.next();
+    }
+    
+}

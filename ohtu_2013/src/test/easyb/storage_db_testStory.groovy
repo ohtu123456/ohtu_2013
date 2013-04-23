0836e57160f3246a9fcc0c4f94c76dccc3f5f3c3
Leif Setala <leif.setala@cs.helsinki.fi>
2013-04-23 20:26:30 +0300

    p

--- ohtu_2013/src/test/easyb/storage_db_testStory.groovy
+++ ohtu_2013/src/test/easyb/storage_db_testStory.groovy
@@ -3,6 +3,23 @@ import com.ohtu123456.ohtu_2013.UserInterface.*
 import com.ohtu123456.ohtu_2013.logic.*
 
 scenario "test",    {
+    given "a database", {
+        db = new StorageDatabase("testidb");
+    }
+
+    when "adding an article", {
+        map = new HashMap<String, String>();
+        map.put("author", "pekka");
+        db.addReference("article", map);
+    }
+
+    then "it should not be empty",   {
+        db.getReferences().size.shouldNotBe 0
+    }
+} 
+
+
+scenario "test",    {
     given "a new program instance", {
         userInput = "-add testidatabase -book a b c d c -quit".split();
         stubio = new StubIO(userInput)

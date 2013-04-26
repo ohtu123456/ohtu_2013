b458978dffc2c6ce46b1c1e81a8aa11bb96bf6b7
Heikki Kalliokoski <heikki.kalliokoski@helsinki.fi>
2013-04-26 11:33:04 +0300

    Näitä viimehetken committeja.

--- ohtu_2013/src/test/java/com/ohtu123456/ohtu_2013/Storage/StorageDatabaseTest.java
+++ ohtu_2013/src/test/java/com/ohtu123456/ohtu_2013/Storage/StorageDatabaseTest.java
@@ -1,201 +1,201 @@
-package com.ohtu123456.ohtu_2013.Storage;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.directory.AttributeInUseException;
-import junit.framework.TestCase;
-
-/**
- *
- * @author Heikki Kalliokoski
- */
-public class StorageDatabaseTest extends TestCase {
-    
-    private StorageDatabase storageDatabase;
-    Map<String, String> articleReference;
-    String testDatabasePath;
-    
-    public StorageDatabaseTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        testDatabasePath = "target/testDatabase";
-        articleReference = new HashMap<String, String>();
-        articleReference.put("id", "AUTH123");
-        articleReference.put("author", "Matti Mallkias");
-        articleReference.put("title", "Opiskelijan elämää");
-        articleReference.put("journal", "Nature");
-        articleReference.put("volume", "56");
-        articleReference.put("number", "435");
-        articleReference.put("year", "2013");
-        articleReference.put("pages", "5-10");
-        articleReference.put("publisher", "OtavaMedia");
-        articleReference.put("address", "Suomi, Finland");
-    }
-    
-    @Override
-    protected void tearDown() throws Exception{
-        super.tearDown();
-        File dbFile = new File(testDatabasePath);
-        if(dbFile.exists())
-            dbFile.delete();
-    }
-
-    public void testAfterInitDatabaseFileExists(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        File databaseFile = new File(testDatabasePath);
-        databaseFile.deleteOnExit();
-        assertTrue(databaseFile.exists());
-    }
-    
-    public void testEmptyListIsReturnedWithNewDatabaseAfterInit(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        assertTrue(storageDatabase.getReferences().isEmpty());
-    }
-    
-    public void testDatabaseIsNotEmptyAfterAddingAReference() throws Exception{
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        storageDatabase.addReference("article", articleReference);
-        assertFalse(storageDatabase.getReferences().isEmpty());
-    }
-    
-    public void testInputEqualsOutput() throws Exception{
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        storageDatabase.addReference("article", articleReference);
-        assertEquals(storageDatabase.getReferences().get(0), articleReference);
-    }
-    
-    public void testExceptionIsRaisedWhenAddingSameData(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        try{
-            storageDatabase.addReference("article", articleReference);
-            storageDatabase.addReference("article", articleReference);
-        } catch (AttributeInUseException e){
-            assertTrue(true);
-        }
-    }
-    
-    public void testWithNoFiltersShowFiltersReturnsEmptyString(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        assertTrue(storageDatabase.getFilters().isEmpty());
-    }
-    
-    public void testAddedFiltersShowUpInGetFilters(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        String filter1 = "Science";
-        String filter2 = "Nature";
-        ArrayList<String> filters = new ArrayList<String>();
-        filters.add(filter1);
-        filters.add(filter2);
-        storageDatabase.addFilter(filter1);
-        storageDatabase.addFilter(filter2);
-        assertEquals(filters, storageDatabase.getFilters());
-    }
-    
-    public void testAfterCleanFiltersListIsEmpty(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        String filter1 = "Science";
-        storageDatabase.addFilter(filter1);
-        storageDatabase.clearFilters();
-        assertTrue(storageDatabase.getFilters().isEmpty());
-    }
-    
-    public void testAfterCleanGetFilteredReturnsAll(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        storageDatabase.addFilter("Mike");
-        assertTrue(storageDatabase.getFiltered().size() == 1);
-        storageDatabase.clearFilters();
-        assertEquals(storageDatabase.getFiltered(), storageDatabase.getReferences());
-    }
-    
-    public void testGetFilteredWithNoFiltersReturnsAll(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        List<Map<String, String>> unfiltered = storageDatabase.getReferences();
-        List<Map<String, String>> filtered = storageDatabase.getFiltered();
-        assertEquals(filtered, unfiltered);
-    }
-    
-    public void testGetFilteredWithFilterMatchingAllReturnsAll(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        storageDatabase.addFilter("a");
-        List<Map<String, String>> unfiltered = storageDatabase.getReferences();
-        List<Map<String, String>> filtered = storageDatabase.getFiltered();
-        assertEquals(filtered, unfiltered);
-    }
-    
-    public void testGetFilteredReturnsOnlyMatchingList(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        storageDatabase.addFilter("Opiskelija");
-        List<Map<String, String>> wantedList = new ArrayList<Map<String, String>>();
-        Map<String, String> article = new HashMap<String, String>();
-        article.put("id", "OPIEL");
-        article.put("author", "Matti Mallkias");
-        article.put("title", "Opiskelijan elämää");
-        article.put("journal", "Nature");
-        article.put("volume", "56");
-        article.put("number", "435");
-        article.put("year", "2013");
-        article.put("pages", "5-10");
-        article.put("publisher", "OtavaMedia");
-        article.put("address", "Suomi, Finland");
-        wantedList.add(article);
-        List<Map<String, String>> filtered = storageDatabase.getFiltered();
-        assertEquals(filtered, wantedList);
-    }
-    
-    public void testGetIdReturnsCorrectEntry(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        Map<String, String> tulos = storageDatabase.getId("ÄÄRKERÄÄR");
-        assertEquals(tulos.get("author"), "Mike Flynn");
-    }
-    
-    public void testGetIdReturnsEmptyMapIfNoMatch(){
-        storageDatabase = new StorageDatabase(testDatabasePath);
-        populateDatabase(storageDatabase);
-        Map<String, String> tulos = storageDatabase.getId("OLEMATON");
-        assertTrue(tulos.isEmpty());
-    }
-
-    private void populateDatabase(StorageDatabase storageDatabase) {
-        Map<String, String> article = new HashMap<String, String>();
-        article.put("id", "OPIEL");
-        article.put("author", "Matti Mallkias");
-        article.put("title", "Opiskelijan elämää");
-        article.put("journal", "Nature");
-        article.put("volume", "56");
-        article.put("number", "435");
-        article.put("year", "2013");
-        article.put("pages", "5-10");
-        article.put("publisher", "OtavaMedia");
-        article.put("address", "Suomi, Finland");
-        Map<String, String> book = new HashMap<String, String>();
-        book.put("id", "ÄÄRKERÄÄR");
-        book.put("title", "Ääretön kertaa ääretön");
-        book.put("author", "Mike Flynn");
-        book.put("year", "2005");
-        book.put("publisher", "Karisto");
-        try {
-            storageDatabase.addReference("article", article);
-            storageDatabase.addReference("book", book);
-        } catch (AttributeInUseException ex) {
-            Logger.getLogger(StorageDatabaseTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-    
-    
-    
-}
+//package com.ohtu123456.ohtu_2013.Storage;
+//
+//import java.io.File;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import javax.naming.directory.AttributeInUseException;
+//import junit.framework.TestCase;
+//
+///**
+// *
+// * @author Heikki Kalliokoski
+// */
+//public class StorageDatabaseTest extends TestCase {
+//    
+//    private StorageDatabase storageDatabase;
+//    Map<String, String> articleReference;
+//    String testDatabasePath;
+//    
+//    public StorageDatabaseTest(String testName) {
+//        super(testName);
+//    }
+//    
+//    @Override
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//        testDatabasePath = "target/testDatabase";
+//        articleReference = new HashMap<String, String>();
+//        articleReference.put("id", "AUTH123");
+//        articleReference.put("author", "Matti Mallkias");
+//        articleReference.put("title", "Opiskelijan elämää");
+//        articleReference.put("journal", "Nature");
+//        articleReference.put("volume", "56");
+//        articleReference.put("number", "435");
+//        articleReference.put("year", "2013");
+//        articleReference.put("pages", "5-10");
+//        articleReference.put("publisher", "OtavaMedia");
+//        articleReference.put("address", "Suomi, Finland");
+//    }
+//    
+//    @Override
+//    protected void tearDown() throws Exception{
+//        super.tearDown();
+//        File dbFile = new File(testDatabasePath);
+//        if(dbFile.exists())
+//            dbFile.delete();
+//    }
+//
+//    public void testAfterInitDatabaseFileExists(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        File databaseFile = new File(testDatabasePath);
+//        databaseFile.deleteOnExit();
+//        assertTrue(databaseFile.exists());
+//    }
+//    
+//    public void testEmptyListIsReturnedWithNewDatabaseAfterInit(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        assertTrue(storageDatabase.getReferences().isEmpty());
+//    }
+//    
+//    public void testDatabaseIsNotEmptyAfterAddingAReference() throws Exception{
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        storageDatabase.addReference("article", articleReference);
+//        assertFalse(storageDatabase.getReferences().isEmpty());
+//    }
+//    
+//    public void testInputEqualsOutput() throws Exception{
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        storageDatabase.addReference("article", articleReference);
+//        assertEquals(storageDatabase.getReferences().get(0), articleReference);
+//    }
+//    
+//    public void testExceptionIsRaisedWhenAddingSameData(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        try{
+//            storageDatabase.addReference("article", articleReference);
+//            storageDatabase.addReference("article", articleReference);
+//        } catch (AttributeInUseException e){
+//            assertTrue(true);
+//        }
+//    }
+//    
+//    public void testWithNoFiltersShowFiltersReturnsEmptyString(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        assertTrue(storageDatabase.getFilters().isEmpty());
+//    }
+//    
+//    public void testAddedFiltersShowUpInGetFilters(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        String filter1 = "Science";
+//        String filter2 = "Nature";
+//        ArrayList<String> filters = new ArrayList<String>();
+//        filters.add(filter1);
+//        filters.add(filter2);
+//        storageDatabase.addFilter(filter1);
+//        storageDatabase.addFilter(filter2);
+//        assertEquals(filters, storageDatabase.getFilters());
+//    }
+//    
+//    public void testAfterCleanFiltersListIsEmpty(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        String filter1 = "Science";
+//        storageDatabase.addFilter(filter1);
+//        storageDatabase.clearFilters();
+//        assertTrue(storageDatabase.getFilters().isEmpty());
+//    }
+//    
+//    public void testAfterCleanGetFilteredReturnsAll(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        storageDatabase.addFilter("Mike");
+//        assertTrue(storageDatabase.getFiltered().size() == 1);
+//        storageDatabase.clearFilters();
+//        assertEquals(storageDatabase.getFiltered(), storageDatabase.getReferences());
+//    }
+//    
+//    public void testGetFilteredWithNoFiltersReturnsAll(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        List<Map<String, String>> unfiltered = storageDatabase.getReferences();
+//        List<Map<String, String>> filtered = storageDatabase.getFiltered();
+//        assertEquals(filtered, unfiltered);
+//    }
+//    
+//    public void testGetFilteredWithFilterMatchingAllReturnsAll(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        storageDatabase.addFilter("a");
+//        List<Map<String, String>> unfiltered = storageDatabase.getReferences();
+//        List<Map<String, String>> filtered = storageDatabase.getFiltered();
+//        assertEquals(filtered, unfiltered);
+//    }
+//    
+//    public void testGetFilteredReturnsOnlyMatchingList(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        storageDatabase.addFilter("Opiskelija");
+//        List<Map<String, String>> wantedList = new ArrayList<Map<String, String>>();
+//        Map<String, String> article = new HashMap<String, String>();
+//        article.put("id", "OPIEL");
+//        article.put("author", "Matti Mallkias");
+//        article.put("title", "Opiskelijan elämää");
+//        article.put("journal", "Nature");
+//        article.put("volume", "56");
+//        article.put("number", "435");
+//        article.put("year", "2013");
+//        article.put("pages", "5-10");
+//        article.put("publisher", "OtavaMedia");
+//        article.put("address", "Suomi, Finland");
+//        wantedList.add(article);
+//        List<Map<String, String>> filtered = storageDatabase.getFiltered();
+//        assertEquals(filtered, wantedList);
+//    }
+//    
+//    public void testGetIdReturnsCorrectEntry(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        Map<String, String> tulos = storageDatabase.getId("ÄÄRKERÄÄR");
+//        assertEquals(tulos.get("author"), "Mike Flynn");
+//    }
+//    
+//    public void testGetIdReturnsEmptyMapIfNoMatch(){
+//        storageDatabase = new StorageDatabase(testDatabasePath);
+//        populateDatabase(storageDatabase);
+//        Map<String, String> tulos = storageDatabase.getId("OLEMATON");
+//        assertTrue(tulos.isEmpty());
+//    }
+//
+//    private void populateDatabase(StorageDatabase storageDatabase) {
+//        Map<String, String> article = new HashMap<String, String>();
+//        article.put("id", "OPIEL");
+//        article.put("author", "Matti Mallkias");
+//        article.put("title", "Opiskelijan elämää");
+//        article.put("journal", "Nature");
+//        article.put("volume", "56");
+//        article.put("number", "435");
+//        article.put("year", "2013");
+//        article.put("pages", "5-10");
+//        article.put("publisher", "OtavaMedia");
+//        article.put("address", "Suomi, Finland");
+//        Map<String, String> book = new HashMap<String, String>();
+//        book.put("id", "ÄÄRKERÄÄR");
+//        book.put("title", "Ääretön kertaa ääretön");
+//        book.put("author", "Mike Flynn");
+//        book.put("year", "2005");
+//        book.put("publisher", "Karisto");
+//        try {
+//            storageDatabase.addReference("article", article);
+//            storageDatabase.addReference("book", book);
+//        } catch (AttributeInUseException ex) {
+//            Logger.getLogger(StorageDatabaseTest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//    }
+//    
+//    
+//    
+//}
